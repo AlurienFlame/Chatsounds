bcc81764bdbd1318a545476543260075ba18d42b
BrailleBennett <103331457+BrailleBennett@users.noreply.github.com>
2025-01-28 00:19:54 -0600

    optimized imports

--- src/main/java/io/github/alurienflame/chatsounds/ModMenuIntegration.java
+++ src/main/java/io/github/alurienflame/chatsounds/ModMenuIntegration.java
@@ -1,8 +1,8 @@
 package io.github.alurienflame.chatsounds;
 
-import me.shedaniel.autoconfig.AutoConfig;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import me.shedaniel.autoconfig.AutoConfig;
 
 public class ModMenuIntegration implements ModMenuApi {
     @Override
